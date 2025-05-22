708473bd42bd9f848a7a378a1fc79dbea2362158
Leo88p <71487423+Leo88p@users.noreply.github.com>
2025-05-22 12:42:34 +0300

    feed

---
+++ Pages/Feed.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab3.Pages
+{
+    public class FeedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Shared/_FeedLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab3.Pages.Shared
+{
+    public class _FeedLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
